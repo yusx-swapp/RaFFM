0b67bd233265cac38a2988aefb7f77226d8ec8f2
yusx-swapp <yusx@iastate.edu>
2023-11-22 10:42:36 -0600

    typo in low-level APIs

--- experiments/comparison_baselines/PriSM/__init__.py
+++ experiments/comparison_baselines/PriSM/__init__.py
@@ -4,4 +4,5 @@ from .prism import (
     deit_small_patch16_224,
     convert_to_orth_model,
     deit_tiny_patch16_224,
+    add_frob_decay,
 )
--- experiments/comparison_baselines/PriSM/prism/__init__.py
+++ experiments/comparison_baselines/PriSM/prism/__init__.py
@@ -8,7 +8,43 @@ from .model import (
 import torch
 from timm.models.layers import PatchEmbed, Mlp
 import copy
-from .nn_transformer import Block_Orth, VisionTransformer_Orth
+from .nn_transformer import Block_Orth, VisionTransformer_Orth, Mlp_Orth, Linear_Orth
+
+import torch
+
+
+def add_frob_decay(model, alpha=0.0001):
+    """
+    Add Frobenius decay to Conv_U/S/V layers
+    Args:
+        model: nn model definition
+        alpha: decay coefficient
+
+    Returns:
+
+    """
+
+    def __recursive_add_decay(module):
+        for m in module.children():
+            if isinstance(m, torch.nn.Sequential):
+                __recursive_add_decay(m)
+            else:
+                # transformer layers
+                if isinstance(m, Block_Orth):
+                    __recursive_add_decay(m)
+                elif isinstance(m, Mlp_Orth):
+                    __recursive_add_decay(m)
+                elif isinstance(m, Linear_Orth):
+                    w_U, w_V = m.fc_U.weight.data, m.fc_V.weight.data
+                    w_U *= m.mask_u.unsqueeze(1)
+                    w_V *= m.mask_v
+                    U_grad = torch.linalg.multi_dot((w_U.T, w_V.T, w_V))
+                    V_grad = torch.linalg.multi_dot((w_U, w_U.T, w_V.T))
+
+                    m.fc_U.weight.grad += alpha * U_grad.T
+                    m.fc_V.weight.grad += alpha * V_grad.T
+
+    __recursive_add_decay(model)
 
 
 def convert_to_orth_model(model, keep, fl=False):
