f6f008665bf57eb08c3fc29b37e5d5c7b1e1eb49
yusx-swapp <yusx@iastate.edu>
2023-11-21 23:52:42 -0600

    fix import issue

--- experiments/comparison_baselines/PriSM/__init__.py
+++ experiments/comparison_baselines/PriSM/__init__.py
@@ -1,1 +1,6 @@
-from .prism import VisionTransformer_Orth, VisionTransformer, deit_small_patch16_224
+from .prism import (
+    VisionTransformer_Orth,
+    VisionTransformer,
+    deit_small_patch16_224,
+    convert_to_orth_model,
+)
