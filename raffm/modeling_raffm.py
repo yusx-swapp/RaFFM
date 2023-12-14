ea9fc595ff253fb30510efa39de625ca7c558fc8
yusx-swapp <yusx@iastate.edu>
2023-12-13 21:53:10 -0600

    Tested experiments in RaFFM version 0.11

--- raffm/modeling_raffm.py
+++ raffm/modeling_raffm.py
@@ -24,10 +24,9 @@ class RaFFM:
         if not elastic_config:
             # set defalt search space configuration (this is defalt setting for bert)
             elastic_config = {
-                # "atten_out_space": [768 - i * 12 for i in range(0, 15)],
                 "atten_out_space": [768],
-                "inter_hidden_space": [3072 - i * 64 for i in range(0, 20)],
-                "residual_hidden_space": [768 - i * 24 for i in range(0, 15)],
+                "inter_hidden_space": [3072, 1920, 1280],
+                "residual_hidden_space": [768],
             }
             print(
                 f"[Warning]: No elastic configuration provides. Set to the defalt elastic space {elastic_config}."
